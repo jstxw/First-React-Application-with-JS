f9ae09e76608af5dd755e1978fbae8a57bc69679
jstxw <ssivfe@Gmail.com>
2025-07-22 02:09:49 -0400

    created context for favorite button

---
+++ frontend/src/Contexts/MovieContext.jsx
@@ -0,0 +1,42 @@
+//state manager
+import { createContext, useState, useContext, useEffect } from "react";
+
+const MovieContext = createContext();
+
+export const useMovieContext = () => useContext(MovieContext);
+export const MovieProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+  //look inside local storage and see if there are already any favorited movies
+  useEffect(() => {
+    const storedFavs = localStorage.getItem("favorites");
+
+    if (storedFavs) setFavorites(JSON.parse(storedFavs)); //local storage can only store strings
+    //edit local storage when favorites change
+    useEffect(() => {
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+    }, [favorites]);
+  }, []);
+
+  const addToFavorites = (movie) => {
+    setFavorites((prev) => [...prev, movie]);
+  };
+
+  const removeFromFavorites = (movieId) => {
+    setFavorites((prev) => prev.filter((movie) => movie.id !== movieId));
+  };
+
+  const isFavorite = (movieId) => {
+    return favorites.some((movie) => movie.id === movieId);
+  };
+
+  const value = {
+    favorites,
+    setFavorites,
+    isFavorite,
+    addToFavorites,
+  };
+
+  return (
+    <MovieContext.Provider value={value}>{children}</MovieContext.Provider>
+  );
+}; // provide state to any of the components wrapped around it
